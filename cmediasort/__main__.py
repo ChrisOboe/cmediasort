53496d39de39c2e0a6e8da94eff92ae5802a84ac
ChrisOboe <ChrisOboe@eml.cc>
2017-04-12 21:22:40 +0200

    refactor nearly complete

--- cmediasort/__main__.py
+++ cmediasort/__main__.py
@@ -18,63 +18,142 @@
 """ the executable for mediasort """
 
 import sys
-import logging
+import os
+import argparse
+import yaml
+from appdirs import user_config_dir
+
 import mediasort
-from . import config
+
+PROG = "cmediasort"
 
 
-def verbose():
-    """ logs info to stdout """
-    log = logging.getLogger()
-    log.setLevel(logging.INFO)
-    log.handlers = []
-    logout = logging.StreamHandler(sys.stdout)
-    logout.setLevel(logging.INFO)
-    log.addHandler(logout)
+def parse_arguments():
+    """Returns a dict of command line arguments."""
+
+    parser = argparse.ArgumentParser(
+        prog=PROG,
+        description='Scrapes metadata for movies and episodes from TMDb '
+        'by guessing the title from scene standard naming conventions.\n'
+        'This product uses the TMDb API but is not endorsed or certified '
+        'by TMDb.')
+    parser.add_argument(
+        "source",
+        help="The file or the folder which should be scanned for files ")
+    parser.add_argument(
+        '-c',
+        '--config',
+        default="{0}/config.yaml".format(user_config_dir(PROG)),
+        help="the config file")
+    parser.add_argument(
+        '-i',
+        '--interactive',
+        action='store_true',
+        help="Asks the user if something is not clear")
+
+    return vars(parser.parse_args())
 
 
-def print_config(config):
-    """ prints the configuration """
-    for entry in config:
-        print("[" + entry + "]")
-        for subentry in config[entry]:
-            if isinstance(config[entry][subentry], list):
-                print(subentry + "=" + ' '.join(config[entry][subentry]))
-            else:
-                print(subentry + "=" + str(config[entry][subentry]))
-        print("")
+def parse_configfile(path):
+    """Returns a dict of settings."""
+
+    if os.path.isfile(path):
+        stream = open(path, 'r')
+        userconfig = yaml.load(stream)
+    else:
+        userconfig = {}
+    return userconfig
+
+
+def find(path, extensions, filesize):
+    """ returns all files with given extensions
+    and bigger than filesize in path """
+
+    filesize *= 1048576  # use filesize as MB
+    if not os.path.exists(path):
+        return
+
+    mediafiles = []
+    if os.path.isfile(path):
+        ext = os.path.splitext(path)[1].lower()[1:]
+        if ext in extensions and os.path.getsize(path) >= filesize:
+            mediafiles.append(path)
+    else:
+        for root, dirs, files in os.walk(path):
+            for filename in files:
+                filepath = os.path.join(root, filename)
+                ext = os.path.splitext(filename)[1].lower()[1:]
+                if (ext not in extensions) or \
+                   (os.path.getsize(filepath) < filesize):
+                    continue
+                mediafiles.append(filepath)
+
+    return mediafiles
+
+
+def identificator_callback(identificator_list, medianame):
+    """ the identificator callback function for interactive mode """
+    c = 1
+    for entry in identificator_list:
+        print("{0}: {1}".format(c, entry['title']))
+        c = c + 1
+    selection = -1
+    while selection < 1 or selection >= c:
+        try:
+            selection = int(input("Select your {0}: ".format(medianame)))
+        except ValueError:
+            pass
+    selection = selection - 1
+    return identificator_list[selection]['id']
 
 
 def main():
     """ Main function for entry point """
     # load config
-    args = config.parse_arguments()
-    settings = config.parse_configfile(args['config'])
+    args = parse_arguments()
+    settings = parse_configfile(args['config'])
 
-    mediasort.tmdb.init(settings['tmdb'])
-    mediasort.fanarttv.init(settings['fanarttv'])
+    # modify settings
+    for mediatype in settings['paths']:
+        if 'template' not in settings['paths'][mediatype]:
+            continue
 
-    # init stuff
-    logging.getLogger("requests").setLevel(logging.WARNING)
-    if args['verbose']:
-        verbose()
+        template = settings['paths'][mediatype]['template']
+        if not template.startswith("/"):
+            settings['paths'][mediatype]['template'] = "{0}/{1}".format(
+                user_config_dir(PROG),
+                template
+            )
 
-    if args['printconfig']:
-        print_config(settings)
-        return()
+    callbacks = {
+        'identificator': None
+    }
+
+    if args['interactive']:
+        callbacks['identificator'] = identificator_callback
+
 
     # get list of files
-    videofiles = mediasort.helpers.find(
+    videofiles = find(
         args['source'],
         settings['videofiles']['allowed_extensions'],
         settings['videofiles']['minimal_file_size'])
 
-    if videofiles is None:
-        print("No videofile found!")
+    if not videofiles:
+        print("No videofile(s) found!")
         sys.exit(0)
 
+    init = mediasort.initialize_plugins(settings)
+    plugins = init['plugins']
+    ids = init['ids']
     for videofile in videofiles:
-        mediasort.sort(videofile, settings)
+        mediasort.sort(videofile,
+                       plugins,
+                       ids,
+                       settings['paths'],
+                       settings['languages'],
+                       settings['overwrite'],
+                       callbacks=callbacks)
 
 
 if __name__ == '__main__':
